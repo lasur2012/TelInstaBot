a3d484ba189c3b6b43850ef37213ba0668f37de9
lasur2012 <74117311+lasur2012@users.noreply.github.com>
2021-01-14 23:13:05 +0300

    Update in_bot.py

--- in_bot.py
+++ in_bot.py
@@ -13,11 +13,34 @@ InstBot.login(username = '---', password = '---')'''
 
 class User_States():
     state = 'Not active'
+    username = 'None'
+    password = 'None'
 user = User_States()
 
+@TelBot.message_handler(commands = ['start']) # start command handler
+def start_handler(message):
+        TelBot.send_message(message.from_user.id, text = f'Hi, {message.from_user.first_name}! To start with lets register in of your Instagram account. Please, enter username')
+        user.state = 'login'
 
+@TelBot.message_handler(content_types = 'text')
+def text_handler(message):
+    if user.state == 'text':
+        TelBot.send_message(message.from_user.id, text = 'Waiting for the text')
 
+    elif user.state == 'login':
+        user.username = str(message.text)
+        TelBot.send_message(message.from_user.id, text = f'OK, your username is {user.username}. Now enter your password')
+        user.state = 'password'
 
+    elif user.state == 'password':
+        user.password = str(message.text)
+        TelBot.send_message(message.from_user.id, text = f'OK, your password is {user.password}. Now you can plan the new post. /now /today /schedule_the_post')
+        user.state = 'log'
+        
+    else:
+        TelBot.send_message(message.from_user.id, text = f'I am not able to speak with you yet, but I will learn it!')
+        
+        
 @TelBot.message_handler(content_types = 'photo')
 def photo_handler(message):
     TelBot.send_message(message.from_user.id, text = 'you sent me a photo')
@@ -40,42 +63,11 @@ def photo_handler(message):
 
 
 '''
-@TelBot.message_handler(commands = ['start']) # start command handler
-def start_handler(message):
-    if str(message.from_user.id) == '225882660':
-        TelBot.send_message(message.from_user.id, text = f'Hi, {message.from_user.first_name}! To start with lets register in of your Instagram account /text /photo')
-    else:
-        TelBot.send_message(message.from_user.id,
-                            text=f'Hi, {message.from_user.first_name}! You are not welcome here. Go away!')
 
-
-@TelBot.message_handler(commands = ['text']) # text command handler
-def enterText_handler(message):
-    user.state = 'text'
-    print(user.state)
-    TelBot.send_message(message.from_user.id, text = f'Hi, {message.from_user.first_name}! Your status was changed on {user.state}')
-
-
-
-@TelBot.message_handler(content_types = 'text')
-def text_handler(message):
-    if user.state == 'text':
-        TelBot.send_message(message.from_user.id, text = f'Waiting for the text')
-    else:
-        TelBot.send_message(message.from_user.id, text = f'I am not able to speak with you yet, but I will learn it!')
-
-
-
-
-
-
-@TelBot.message_handler(commands = ['post'])
+@TelBot.message_handler(commands = ['now'])
 def post(message):
     InstBot.upload_photo('HI.jpg', caption = 'Today is the first day of bot creating)')
     TelBot.send_message(message.from_user.id, text = f'It worked out. I hope..')
-
-
-
 
 
 '''
